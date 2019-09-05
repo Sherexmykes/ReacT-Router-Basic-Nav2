a5c9582e4e6a5918875b51e31a768d53121d7f12
Sheila R Moore <sherexmykes@github.com>
2019-09-04 20:04:13 -0400

    nav component

---
+++ src/Navigation.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Navigation = () => {
+  return (
+    <div>
+      <div className="App">
+        <h1>React Router App</h1>
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <Link to="/about">About</Link>
+        </div>
+        <div>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
